a4e8e0a6114865b6505ab21c99a3f19460d08614
agisboye <august@heegaard.org>
2022-02-10 13:42:03 +0100

    Remove temp files

--- tmp2.js
+++
@@ -1,7 +0,0 @@
-const { basename, dirname, join } = require("path")
-
-const {LOCAL_KEYRING_PATH } = require("./constants")
-
-console.log(dirname(LOCAL_KEYRING_PATH))
-console.log(basename(LOCAL_KEYRING_PATH))
-console.log(join(dirname(LOCAL_KEYRING_PATH), "", basename(LOCAL_KEYRING_PATH)))
