4fe24fa38795e4cdad5b6cff10d15e3f1b4a73a5
agisboye <august@heegaard.org>
2022-02-10 10:23:03 +0100

    Format

--- fuse/file-handle.js
+++ fuse/file-handle.js
@@ -9,7 +9,7 @@ const {
     TOTAL_SIGNATURE_SIZE,
     CAPABILITY_TYPE_READ,
     CAPABILITY_TYPE_WRITE,
-    CAPABILITY_TYPE_VERIFY,
+    CAPABILITY_TYPE_VERIFY
 } = require("../constants")
 
 class FileHandle {
