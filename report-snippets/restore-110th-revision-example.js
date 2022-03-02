1131d4a947bef5ba43d9d486a7e7776f4edafd6f
Jakob Offersen <jakoboffersen@gmail.com>
2022-03-02 09:27:32 +0100

    fix restore-snippet example

--- report-snippets/restore-110th-revision-example.js
+++ report-snippets/restore-110th-revision-example.js
@@ -1,33 +1,43 @@
-// This snippet shows how Dropbox revisions work.
-// What the snippet does:
-//  1) Creates 3 revisions of a file
-//  2) Prints all 3 revisions
-//  3) Restores the file back to its second revisions
-//  3) Prints the file content, which is the second revision
-//  4) Prints all 4 revisions.
-//     This step shows that restoring a file also prepends a unique ID to the revision log.
+// This snippet shows that it is possible to restore a revision for a file
+// that has more than 100 newer revisions to it.
+// According to Dropbox API doc https://www.dropbox.com/developers/documentation/http/documentation#files-list_revisions
+// it is at most possible to list the 100 newest revisions
 
 const { Dropbox } = require("dropbox")
-const dbx = new Dropbox({ accessToken: "your-access-token-here" })
-const remoteFilePath = "/test-file.txt"
+const { FSP_ACCESS_TOKEN } = require("../constants")
 
+const dbx = new Dropbox({ accessToken: FSP_ACCESS_TOKEN })
+const path = "/test-revisions.txt"
 ;(async () => {
-    // writes "r1", then "r2" then "r3" to the remote file to create 3 revisions of the file
-    for (const version of ["r1", "r2", "r3"]) {
-        await dbx.filesUpload({ path: remoteFilePath, mode: "overwrite", contents: version })
+
+    // Write to a remote file: 0, 1, then 2 ... up to 9.
+    for (let i = 0; i < 10; i++) {
+        await dbx.filesUpload({ path, mode: "overwrite", contents: i })
+        console.log(i)
     }
-    // downloads and prints the 3 revisions
-    const entries1 = (await dbx.filesListRevisions({ path: remoteFilePath, mode: "path" })).result.entries
-    console.log(entries1.map(entry => entry.rev)) // ['015d70627251361...', '015d706271a07ac...', '015d706270b981f...']
 
-    // Restore the file back to "r2"
-    await dbx.filesRestore({ path: remoteFilePath, rev: entries1[1].rev })
+    // Fetch the revisions for the 10 writes and save the oldest.
+    const resp1 = await dbx.filesListRevisions({ path: path, limit: 10 })
+    const revs = resp1.result.entries
+    console.log("revs count:", revs.length)
+    const firstRev = revs[revs.length - 1]
 
-    // downloads the current file and prints it
-    const response = await dbx.filesDownload({ path: remoteFilePath })
-    console.log(response.result.fileBinary.toString()) // "r2"
+    console.log(`first rev: ${firstRev.rev}`)
 
-    // downloads and prints the 4 revisions
-    const entries2 = (await dbx.filesListRevisions({ path: remoteFilePath, mode: "path" })).result.entries
-    console.log(entries2.map(entry => entry.rev)) // ['015d70627910d41...', '015d70627251361...', '015d706271a07ac...', '015d706270b981f...']
+    // Make 100 more writes to the remote path: 10, then 11, then 12 ... up to 110
+    for (let i = 10; i <= 110; i++) {
+        await dbx.filesUpload({ path, mode: "overwrite", contents: i })
+        console.log(i)
+    }
+
+    // Request FSP to restore the first of the 110 writes.
+    console.log("try restore to first rev...")
+    await dbx.filesRestore({ path: path, rev: firstRev.rev })
+
+    // Download the current revision. We expect this to contain "0"
+    console.log("try download current revision...")
+    const resp3 = await dbx.filesDownload({ path: path })
+
+    console.log(resp3.result.fileBinary.toString()) // is "0"
+
 })()
